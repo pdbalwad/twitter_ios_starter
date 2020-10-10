e4740aa114f47280b660febd3ef55d849fb93f4d
Priyanka Balwadkar <pdbalwad@syr.edu>
2020-10-09 23:20:46 -0400

    Tweet Cell designed

--- Twitter/HomeViewController.swift
+++ Twitter/HomeViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class HomeViewController: UITableViewController {
 
+    var tweetArray = [NSDictionary]()
+    var numberOfTweets: Int!
+    
+    
     @IBAction func onLogoutButtonClick(_ sender: Any) {
         print("Logout button clicked")
         TwitterAPICaller.client?.logout()
@@ -28,17 +32,25 @@ class HomeViewController: UITableViewController {
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "tweetCell", for: indexPath) as! TweetCell
+        
+        
+        return cell
+    }
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return 5
     }
 
     /*
